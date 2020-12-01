840abc1ee67aa436a2aa215049c4772623211bcc
lihaoze <lihaoze.cq@bytedance.com>
2020-12-01 17:35:33 +0800

    init debugger

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -2,5 +2,6 @@ import Form from "./components/Form";
 import withTheme from "./withTheme";
 import * as utils from "./utils";
 
+console.log("debugger!");
 export { withTheme, utils };
 export default Form;
--- packages/playground/src/index.js
+++ packages/playground/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Theme as MuiTheme } from "@rjsf/material-ui";
 import { Theme as FluentUITheme } from "@rjsf/fluent-ui";
-import { Theme as SuiTheme } from "@rjsf/semantic-ui";
+// import { Theme as SuiTheme } from "@rjsf/semantic-ui";
 import { Theme as AntdTheme } from "@rjsf/antd";
 import { Theme as Bootstrap4Theme } from "@rjsf/bootstrap-4";
 import Playground from "./app";
@@ -95,11 +95,11 @@ const themes = {
     stylesheet: "//cdnjs.cloudflare.com/ajax/libs/antd/4.1.4/antd.min.css",
     theme: AntdTheme,
   },
-  "semantic-ui": {
-    stylesheet:
-      "//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css",
-    theme: SuiTheme,
-  },
+  // "semantic-ui": {
+  //   stylesheet:
+  //     "//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css",
+  //   theme: SuiTheme,
+  // },
   "fluent-ui": {
     stylesheet:
       "//static2.sharepointonline.com/files/fabric/office-ui-fabric-core/11.0.0/css/fabric.min.css",
